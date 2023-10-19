76bd077a64aa2492a4a4a30d1ab46048caa2a75f
Courtney Koko <courtney3koko@yahoo.com>
2023-10-19 15:39:55 -0500

    remove import

--- pages/quotes/auto.tsx
+++ pages/quotes/auto.tsx
@@ -1,4 +1,3 @@
-import JotFormEmbed from 'react-jotform-embed';
 import AutoQuoteHero from '../../components/Content/Hero/AutoQuoteHero';
 import HeroImg from "../../public/assets/images/quotes/auto/Hero.png";
 import NumbersSection from '../../components/Content/FlexDigrams/NumbersSection';
