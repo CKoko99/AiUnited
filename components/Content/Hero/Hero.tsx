7b3c5b2ea52327f308c45a7d9ea4a7e0ade27060
Courtney Koko <courtney3koko@yahoo.com>
2023-10-26 08:08:10 -0500

    Add normal title subtitle hero

---
+++ components/Content/Hero/Hero.tsx
@@ -0,0 +1,95 @@
+import { Box, Typography, Button, TextField } from "@mui/material";
+import Image from "next/image";
+import { Lang } from "../../locale/LocaleSwitcher";
+import { useRouter } from "next/router";
+const styles = {
+    imageContainer: {
+        width: "100%",
+        minHeight: { xs: "32rem", sm: "29rem", md: "24rem" },
+
+        position: "relative",
+    },
+    textContainer: {
+        top: "50%", position: "absolute",
+        left: "50%", transform: "translate(-50%, -50%)",
+        width: { xs: "80%", sm: "70%", md: "60%" },
+        color: "white",
+        textAlign: "center",
+    },
+    glowText: {
+        textShadow: " 0 0 10px #c1c1c1, 0 0 1px #a1a1a1, 0 0 30px #a8a8a8, 0 0 40px #a38d8f, 0 0 50px #b25e65, 0 0 60px #bb4a53, 0 0 70px #e27b83;"
+
+    },
+    glowTextSub: {
+        textShadow: "0 0 10px #fff, 0 0 20px #000, 0 0 30px #a8a8a8, 0 0 40px #a38d8f, 0 0 50px #b25e65, 0 0 60px #bb4a53, 0 0 70px #e27b83;"
+    },
+    Buttons: {
+        display: "flex",
+        flexDirection: { xs: "column", md: "row" },
+        justifyContent: "space-around",
+        alignItems: "center",
+        margin: "2rem 0 0",
+
+        gap: "1rem",
+    }
+}
+const errorValidationText = {
+    en: "Please enter a valid zipcode",
+    es: "Por favor ingrese un código postal válido"
+}
+export default function (props) {
+    const router = useRouter()
+    const { locale } = router
+    const currentLang = Lang[locale ?? 'en']
+
+    return (
+        <>
+            <Box
+                sx={{
+                    position: "relative",
+                }}
+            >
+
+                <Box
+                    sx={{
+                        backgroundColor: "black",
+                    }}
+                >
+                    <Box
+                        sx={{
+                            ...styles.imageContainer,
+                            opacity: props.opacity ? props.opacity : .5,
+                            backgroundColor: "black",
+                            overflow: "hidden",
+                        }}>
+                        <Image {...props.image} fill style={{ objectFit: "cover" }} />
+                    </Box>
+                </Box>
+                <Box
+                    sx={{
+                        ...styles.textContainer,
+                        textAlign: props.align ? props.align : "center",
+                    }}
+                >
+                    <Typography variant="h3"
+                        sx={{
+                            ...styles.glowText,
+
+                        }}
+                    >
+                        {props.title[currentLang]}
+                    </Typography>
+                    <Typography variant="h4"
+
+                        sx={{
+                            ...styles.glowTextSub,
+
+                        }}
+                    >
+                        {props.subtitle[currentLang]}
+                    </Typography>
+                </Box>
+            </Box>
+        </>
+    )
+}
