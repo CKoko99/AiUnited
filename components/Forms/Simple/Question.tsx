2e271ab8a3648b3deee74fdf17c4a0803e5a53f3
Courtney Koko <courtney3koko@yahoo.com>
2023-11-09 14:49:24 -0600

    Add file size limits

--- components/Forms/Simple/Question.tsx
+++ components/Forms/Simple/Question.tsx
@@ -1,4 +1,4 @@
-import { Box, FormControl, TextField, Input, Typography, RadioGroup, Radio, FormControlLabel, Select, MenuItem, InputLabel, FormHelperText, Button } from "@mui/material"
+import { Box, FormControl, TextField, Input, Typography, RadioGroup, Radio, FormControlLabel, Select, MenuItem, InputLabel, FormHelperText, Button, Modal } from "@mui/material"
 import { useRouter } from "next/router"
 import { Lang } from "../../locale/LocaleSwitcher"
 import { ChangeEvent, useEffect, useMemo, useState } from "react"
@@ -7,6 +7,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import dayjs, { Dayjs } from 'dayjs';
 import { CustomFonts } from "../../../providers/theme"
 import { KeyboardArrowDownSharp } from "@mui/icons-material"
+import CloseIcon from '@mui/icons-material/Close';
 import PATHCONSTANTS from "../../../constants/sitemap"
 
 const chunkSize = 1024 * 1024 * .5;
@@ -46,6 +47,25 @@ const validationText = {
 const selectLabel = {
     en: "Select",
     es: "Seleccione",
+}
+const classes = {
+    modal: {
+        position: "fixed",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%,-50%)",
+        zIndex: 1002,
+        width: { xs: "90%", sm: "80%", md: "70%", lg: "50%" },
+        minHeight: "20%",
+        backgroundColor: "white",
+        borderRadius: "10px",
+        display: "flex",
+        flexDirection: "column",
+        gap: "1rem",
+        padding: { xs: "1rem", sm: " 2rem  " },
+        boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
+        maxHeight: "90vh",
+    },
 }
 function InputQuestion(props) {
     const router = useRouter()
@@ -400,12 +420,25 @@ function FileQuestion(props) {
     const [value, setValue] = useState({})
     const [currentChunkIndex, setCurrentChunkIndex] = useState(-1);
     const [progress, setProgress] = useState(0);
-    const [fileLink, setFileLink] = useState("")
+    const [showModal, setShowModal] = useState(false)
+
     function handleDrop(e: any) {
         e.preventDefault();
         console.log(e.target.files[0]);
-        props.setValid(props.index, true)
-        setValue(e.target.files[0])
+        if (!e.target.files[0]) {
+            return
+        }
+        if (e.target.files[0].size / 1024 / 1024 > 10) {
+            props.setValid(props.index, false)
+            setValue({})
+            setShowModal(true)
+            return
+        }
+        if (e.target.files[0]) {
+            console.log(e.target.files[0].size / 1024 / 1024)
+            props.setValid(props.index, true)
+            setValue(e.target.files[0])
+        }
     }
 
 
@@ -528,6 +561,21 @@ function FileQuestion(props) {
                 {value && (value as File).name}
             </Box>
         </Box>
+        <Modal open={showModal} onClose={() => setShowModal(false)}>
+            <Box
+                sx={{ ...classes.modal }}
+            >
+                <Box
+                    sx={{ textAlign: "right" }}
+                >
+                    <CloseIcon sx={{ cursor: "pointer" }} onClick={() => setShowModal(false)} />
+                </Box>
+                <Typography textAlign={"center"} variant="h6">
+                    The Selected file is too large please select a file under 10MB.
+                </Typography>
+                <Button onClick={() => setShowModal(false)}>Close</Button>
+            </Box>
+        </Modal>
     </>
 }
 export default function (props) {
