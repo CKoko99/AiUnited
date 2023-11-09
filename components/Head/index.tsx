c4aa2f3bfd188d7d51950fe3450f7e4e7321760c
Courtney Koko <courtney3koko@yahoo.com>
2023-11-09 11:39:48 -0600

    Add HTML body tag

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -5,12 +5,13 @@ type HeadComponentPropsType = {
   metaData: string;
 }
 
-const HeadComponent = ({title, metaData}: HeadComponentPropsType) => {
+const HeadComponent = ({ title, metaData }: HeadComponentPropsType) => {
   return (
     <Head>
       <title>{title}</title>
       <meta name="description" content={metaData} />
       <link rel="icon" href="/favicon.ico" />
+      <meta name="google-site-verification" content="eBcP0RUizk4nbWiQlzGJKWaFZaPuYTm9Fzo2S954WJE" />
     </Head>
   )
 }
