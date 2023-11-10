ad28ee239e76095a52bd88e0164298cbc47b1a62
Courtney Koko <courtney3koko@yahoo.com>
2023-11-10 08:28:57 -0600

    favicon

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -10,7 +10,8 @@ const HeadComponent = ({ title, metaData }: HeadComponentPropsType) => {
     <Head>
       <title>{title}</title>
       <meta name="description" content={metaData} />
-      <link rel="icon" href="/favicon.ico" />
+      {//<link rel="icon" href="/favicon.ico" />
+      }
       <meta name="google-site-verification" content="eBcP0RUizk4nbWiQlzGJKWaFZaPuYTm9Fzo2S954WJE" />
     </Head>
   )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,11 +3,15 @@ import type { AppProps } from 'next/app'
 import Layout from "../components/Layout";
 import { ThemeProvider } from '@emotion/react';
 import theme from "../providers/theme";
+import Head from 'next/head';
 
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+      <Head>
+        <link rel="shortcut icon" href="/images/favicon.ico" />
+      </Head>
       <Layout>
         <Component {...pageProps} />
 
