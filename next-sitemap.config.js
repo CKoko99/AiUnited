6ed91377c5836beafb5cb44d901d17d1446a6969
Courtney Koko <courtney3koko@yahoo.com>
2023-11-21 10:37:32 -0600

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://aiunited-uc3v53rceq-ue.a.run.app',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+}
