3464332b5d8b791fa12ec58359cb4f6fd3a5d1a9
Courtney Koko <courtney3koko@yahoo.com>
2023-11-21 10:37:24 -0600

    g tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,31 @@ import type { AppProps } from 'next/app'
 import Layout from "../components/Layout";
 import { ThemeProvider } from '@emotion/react';
 import theme from "../providers/theme";
+import Script from 'next/script';
 
-
+const Gtag = ""
 function MyApp({ Component, pageProps }: AppProps) {
+
   return (
     <ThemeProvider theme={theme}>
       <Layout>
         <Component {...pageProps} />
-
+        <Script
+          strategy='afterInteractive' src={`https://www.googletagmanager.com/gtag/js?id=${Gtag}`}
+        />
+        <Script
+          strategy='afterInteractive'
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments)}
+              gtag('js', new Date());
+              gtag('config', '${Gtag}', {
+                page_path: window.location.pathname,
+              });
+            `,
+          }}
+        />
       </Layout>
     </ThemeProvider>
   )
