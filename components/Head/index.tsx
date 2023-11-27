c53a25deb6e30c99d534165a2ed3c150592dc29f
Courtney Koko <courtney3koko@yahoo.com>
2023-11-27 09:25:07 -0600

    Update meta creator and publisher

--- components/Head/index.tsx
+++ components/Head/index.tsx
@@ -11,6 +11,8 @@ const HeadComponent = ({ title, metaData }: HeadComponentPropsType) => {
     <Head>
       <GTMHead />
       <title>{title || "Ai United"}</title>
+      <meta name="creator" content="Ai United" />
+      <meta name="publisher" content="Ai United" />
       <meta name="description" content={metaData || "Ai United Insurance provides you with coverage options, benefits and discounts at a price you can afford."} />
       <link rel="icon" href="https://firebasestorage.googleapis.com/v0/b/insurance-hub-397016.appspot.com/o/ai-favicon.ico?alt=media&token=192149a1-59c8-48d2-be99-5f751ab91e1a" />
       <meta name="google-site-verification" content="eBcP0RUizk4nbWiQlzGJKWaFZaPuYTm9Fzo2S954WJE" />
