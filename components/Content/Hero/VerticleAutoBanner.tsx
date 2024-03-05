389adb76b03d2840a3b8d204aef6ee5b238dc068
Courtney Koko <courtney3koko@yahoo.com>
2024-03-05 11:45:52 -0600

    Add Custom Button Tracking

--- components/Content/Hero/VerticleAutoBanner.tsx
+++ components/Content/Hero/VerticleAutoBanner.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import theme from "../../../providers/theme";
 import { GTMEVENTS, GTMEventHandler } from "../../Scripts/GoogleTag";
 import { useEffect, useState } from "react";
+import PATHCONSTANTS from "constants/sitemap";
 
 const errorValidationText = {
     en: "Please enter a valid zipcode",
@@ -24,6 +25,36 @@ export default function (props) {
     const [onceValid, setOnceValid] = useState(false)
     let alinscoLink = `https://customers.empowerins.com/Fn/Quotes/PrimaryDriver.aspx?AgentNo=p1cLqpHj4s4fcL2mzmkBvA&zipcode=${inputValue}`
 
+    async function uploadToSheet() {
+        const timestamp = new Date().toLocaleString();
+
+        const formData = new FormData();
+        formData.append("1 Timestamp", timestamp);
+        formData.append("2 Device", window.navigator.userAgent);
+        formData.append("3 ZipCode", inputValue);
+        formData.append("SheetTitle", "Ai United Alinsco Auto Banner");
+        formData.append("Spreadsheet", "AiUnited");
+        try {
+            await fetch(`${PATHCONSTANTS.BACKEND}/forms`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(
+                    [...formData.entries(),]
+                ),
+            })
+                .then((res) => res.json())
+                .then((data) => {
+                    console.log(data);
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        } catch (error) {
+            console.log(error)
+        }
+    }
 
     const handleValueChange = (targetValue) => {
         if (props.validation?.toLowerCase() === "zipcode") {
@@ -33,14 +64,17 @@ export default function (props) {
             setInputValue(targetValue)
         }
     }
-    function redirect() {
+
+    async function redirect() {
         if (isValid) {
+            uploadToSheet()
             GTMEventHandler(`${GTMEVENTS.conversion}-Auto`, { 'name': "Auto-Quote" })
             window.open(alinscoLink, "_blank")
         } else {
             setErrorText(errorValidationText[currentLang])
         }
     }
+
     useEffect(() => {
         if (props.validation.toLowerCase() === "zipcode") {
             if (inputValue?.length === 5) {
@@ -99,11 +133,7 @@ export default function (props) {
                         variant="contained"
                         color="secondary"
                         onClick={() => {
-                            if (isValid) {
-                                window.open(alinscoLink, "_blank")
-                            } else {
-                                setErrorText(errorValidationText[currentLang])
-                            }
+                            redirect()
                         }}
                         sx={{
                             minWidth: "10rem", minHeight: "3.5rem"
