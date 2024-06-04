e1841fd40e8301a80a610eabc77fd5d79def5693
Courtney Koko <courtney3koko@yahoo.com>
2024-06-04 13:39:27 -0500

    Create auto-insurance-quote.html.tsx

---
+++ pages/auto-insurance-quote.html.tsx
@@ -0,0 +1,11 @@
+import PATHCONSTANTS from "constants/sitemap";
+import { useEffect } from "react";
+
+export default function () {
+  //redirect to /about/careers
+  useEffect(() => {
+    window.location.href = PATHCONSTANTS.QUOTES.AUTO.INDEX;
+  }, []);
+
+  return <></>;
+}
